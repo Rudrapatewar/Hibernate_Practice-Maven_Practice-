7501fad0706846e61c2ad9a3851c210c596275d6
Rudra <patewarrudra@gmail.com>
2026-08-20 01:00:27 +0530

    1st hibernate program added

--- src/main/java/com/rudra/App.java
+++ src/main/java/com/rudra/App.java
@@ -1,4 +1,11 @@
+
 package com.rudra;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 /**
  * Hello world!
@@ -6,8 +13,31 @@ package com.rudra;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Project Started");
+
+
+        Configuration cfg = new Configuration();
+        cfg.configure();
+        SessionFactory factory = cfg.buildSessionFactory();
+
+        //creating student
+        Student st =new Student();
+        st.setId(102);
+        st.setName("Rudra");
+        st.setCity("Pune");
+        System.out.println(st);
+
+        //Session session =factory.getCurrentSession();
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.persist(st);
+
+        tx.commit();
+
+        session.close();
+
+
     }
 }
